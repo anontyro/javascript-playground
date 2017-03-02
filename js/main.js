69ebf2d8114411f1d9d2bc59978f2ba2f93332d3
anontyro <alexwilkinson@gmail.com>
2017-03-02 23:36:03 +0800

    DOM manipulation lists
    Adding items to lists and recording how many

--- js/main.js
+++ js/main.js
@@ -8,6 +8,16 @@ firstBtn.addEventListener("click", function () {
 
 midBlock.addEventListener("click", function (e) {
 	var child = midBlock.firstChild;
-	console.log(e);
-	// while(child != )
+
+
+	var node = document.createElement("LI");
+	var textNode = document.createTextNode(e.target.textContent);
+	node.appendChild(textNode);
+	document.getElementById("my-list").appendChild(node);
+
+
+	var totalList = document.getElementById("my-list").getElementsByTagName("li").length;
+	document.getElementById("len-total").textContent = totalList;
+	console.log("Length of my-list: " + totalList );
+
 });
